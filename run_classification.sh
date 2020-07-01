eefeea1979a5ba5a258307ae71b4139c67545cf9
Sarah Wooders <sarahwooders@gmail.com>
2020-07-01 05:12:28 +0000

    adding sarah's copy of uma's doordash preprocessing

--- run_classification.sh
+++ run_classification.sh
@@ -1,9 +1,22 @@
 #wandb init;
 
 # Run training
+#python examples/text-classification/run_doordash.py \
+#  --data_dir="/home/transformers-public/data_dir/" \
+#  --model_name_or_path="/home/transformers-public/dish-type-model/checkpoint-70000" \
+#  --run_name="sarah-test8" \
+#  --max_seq_length=64 \
+#  --per_gpu_train_batch_size=32 \
+#  --do_train \
+#  --logging_steps=100 \
+#  --writing_dir="/home/transformers-public" \
+#  --max_steps=100000 \
+#  --save_steps=2000 \
+#  --tokenized_root_dir="/home/doordash-by-dish-no-and/title-tokenized/bert-base-multilingual-cased"
+
 python examples/text-classification/run_doordash.py \
   --data_dir="/home/transformers-public/data_dir/" \
-  --model_name_or_path="/home/transformers-public/dish-type-model/checkpoint-70000" \
+  --model_name_or_path="bert-base-multilingual-cased" \
   --run_name="sarah-test8" \
   --max_seq_length=64 \
   --per_gpu_train_batch_size=32 \
@@ -13,17 +26,4 @@ python examples/text-classification/run_doordash.py \
   --max_steps=100000 \
   --save_steps=2000 \
   --tokenized_root_dir="/home/doordash-by-dish-no-and/title-tokenized/bert-base-multilingual-cased"
-
-#python examples/text-classification/run_doordash.py \
-#  --data_dir="/home/transformers-public/data_dir/" \
-#  --model_name_or_path="bert-base-multilingual-cased" \
-#  --run_name="sarah-test7" \
-#  --max_seq_length=64 \
-#  --per_gpu_train_batch_size=32 \
-#  --do_train \
-#  --logging_steps=100 \
-#  --writing_dir="/home/transformers-public" \
-#  --max_steps=100000 \
-#  --save_steps=2000 \
-#  --tokenized_root_dir="/home/doordash-by-dish-no-and/title-tokenized/bert-base-multilingual-cased"
 
